fb4abdaddcea6ddd95e167b92e3f056f9e31a227
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-09-30 17:11:35 +0200

    add orderOption

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import styles from './OrderOption.scss';
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import PropTypes from 'prop-types';
 
-const OrderOption = () => (
-  <div className={styles.component}>
-    <h3 className={styles.title}>Car Rental</h3>
-  </div>
-);
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
+
+OrderOption.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+};
 
 export default OrderOption;
---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderOptionIcon = () => (
+  <div>
+        OrderOptionIcon
+  </div>
+);
+
+export default OrderOptionIcon;
