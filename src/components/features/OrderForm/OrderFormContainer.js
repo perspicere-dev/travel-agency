3a6a78b3a2ae6a43fde894b1324b23ac60a29c62
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-09-30 13:34:25 +0200

    add calculate total price by functions in orderSummary

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import OrderForm from './OrderForm';
-import getOrderOptions from '../../../redux/orderRedux';
+import {getOrderOptions} from '../../../redux/orderRedux';
 
 
 const mapStateToProps = (state) => ({
