45749fd5115379b78b02e5009d4ebcbfe4d1d73c
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-10-12 20:57:34 +0200

    add testing for TripSummary

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -3,21 +3,47 @@ import {shallow} from 'enzyme';
 import TripSummary from './TripSummary';
 
 describe ('Component TripSummary', () => {
+  const expectedId = 'abc';
+
   it('should render without crashing', () => {
-    const expectedId = 'abc';
     const component = shallow(<TripSummary tags={[]} id={expectedId}/>); 
     expect(component).toBeTruthy();
-    console.log(component.debug());
-
   });
 
   it('should render link to proper adrss if props id=abc', () => {
     const expectedId = 'abc';
     const generatedLink = `/trip/${expectedId}`;
-    const component = shallow(<TripSummary tags={[]} id='abc' />);
+    const component = shallow(<TripSummary tags={[]} id={expectedId} />);
 
     expect(component.find('.link').prop('to')).toEqual(generatedLink);
   });
+
+  it('should get valid src and alt for <img>', () => {
+    const expectedImgOrName = 'string';
+    const component = shallow(<TripSummary tags={[]} id={expectedId} image={expectedImgOrName} name={expectedImgOrName}/>);
+    expect(component.find('img').prop('src')).toEqual(expectedImgOrName);
+    expect(component.find('img').prop('alt')).toEqual(expectedImgOrName);
+    console.log(component.debug());
+  });
+
+  it('should render corectly props name, cost, days', () => {
+    const expectedName = 'name';
+    const expectedDays = 123;
+    const expectedCost = 'cost';
+    const component = shallow(<TripSummary tags={[]} id={expectedId} name={expectedName} cost={expectedCost} days={expectedDays}/>);
+
+    const renderedName = component.find('.title').text();
+    const renderedDays = component.find('.details').childAt(0).text();
+    const renderedCost = component.find('.details').childAt(1).text(); 
+    expect(renderedName).toEqual(expectedName);
+    expect(renderedDays).toEqual(expectedDays + ' days');
+    expect(renderedCost).toEqual('from ' + expectedCost);
+  });
+
+  it('should throw error without requierd props', () => {
+    expect(() => shallow(<TripSummary tags={[]} />)).toThrow();
+  });
+  
 });
     
 
